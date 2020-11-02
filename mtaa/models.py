83027b6ce010633a10b21afeac3f04e1ff66fd4e
ibukamshindi <ibukamshindi@gmail.com>
2020-11-03 02:55:47 +0300

    add tests methods

--- mtaa/models.py
+++ mtaa/models.py
@@ -10,17 +10,65 @@ class Hood(models.Model):
     hood_name=models.CharField(max_length=50,blank=True)
     hood_photo=models.ImageField(upload_to='hoodpics/')
     population=models.CharField(max_length=50,blank=True)
+
+    def __str__(self):
+        return self.hood_name
+
+    def create_hood(self):
+        self.save()
+
+    def delete_hood(self):
+        self.delete()
+
+    def find_neighborood(self,hood_id):
+        self.search_by_id(id=hood_id)
+
+    def update_neighborhood(self):
+        self.update_hood()
+    
+    def update_occupants(self):
+        self.update_population()
+        
 class Profile(models.Model):
     name=models.CharField(max_length=50,blank=True)
     user_id=models.OneToOneField(User,null=True,on_delete=models.CASCADE)
     neighborhood=models.ForeignKey(Hood,null=True,on_delete=models.CASCADE)
     email_address=models.CharField(max_length=50,blank=True)
 
+    def __str__(self):
+      return self.name
+    
+    def save_user(self):
+        self.save()
+
+    def create_user_profile(sender,instance,created,**kwargs):
+        if created:
+            Profile.objects.create(user_id=instance)
+
+    def save_profile(sender,instance,**kwargs):
+        instance.profile.save()    
 class Business(models.Model):
     business_name=models.CharField(max_length=50,blank=True)
     email=EmailField(null=False)
     user=models.ForeignKey(User,null=True,on_delete=models.Case)
     hood=models.ForeignKey(Hood,null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hood
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    def find_business(self,business_id):
+        self.search_by_id(id=business_id)
+
+    def update_business(self):
+        self.update()
+
+    
 
 class Notification(models.Model):
     post=models.CharField(max_length=50,blank=True)
--- mtaa/tests.py
+++ mtaa/tests.py
@@ -3,7 +3,7 @@ from .models import Hood,Profile,Business
 # Create your tests here.
 class ProfileTestClass(TestCase):
     def setUp(self):
-        self.profile=Profile(name='patrick',neighborhood='ruiru',email_address="patode01@gmail.com")
+        self.profile=Profile(name='patrick',email_address="patode01@gmail.com")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile,Profile))
@@ -12,3 +12,22 @@ class ProfileTestClass(TestCase):
         self.profile.save_profile()
         profiles=Profile.objects.all()
         self.assertTrue(len(profiles)>0)
+
+class BusinessTestClass(TestCase):
+    def setUp(self):
+        self.business=Business(business_name='Joes Plumbing',email="joeplum@gmail.com")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business,Business))
+
+    def test_create_method(self):
+        self.business.create_business()
+        business=Business.objects.all()
+        self.assertTrue(len(business)>0)
+
+    def test_delete_method(self):
+        self.business.create_business()
+        business = Business.objects.all()
+        self.business.delete_business()
+        business = Business.objects.all()
+        self.assertTrue(len(business)==0)      
