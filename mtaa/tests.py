a89f89cd25539ede73de50b102a1bca8ff924278
ibukamshindi <ibukamshindi@gmail.com>
2020-11-02 11:51:50 +0300

    add profile test create migrations

--- mtaa/tests.py
+++ mtaa/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Hood,Profile,Business
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile=Profile(name='patrick',neighborhood='ruiru',email_address="patode01@gmail.com")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
