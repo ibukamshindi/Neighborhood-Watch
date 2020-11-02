0f7e0bbbd375bccfae9c3a53a4ea36a2b56b97e0
ibukamshindi <ibukamshindi@gmail.com>
2020-11-02 04:11:08 +0300

    add views home add models

--- mtaa/models.py
+++ mtaa/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import EmailField
+from django.dispatch import receiver
+
+
 
 # Create your models here.
+class Profile(models.Model):
+    name=models.CharField(max_length=50,blank=True)
+    user_id=models.OneToOneField(User,null=True,on_delete=models.CASCADE)
+    neighborhood=models.ForeignKey(Hood,null=True,on_delete=models.CASCADE)
+    email_address=models.CharField(max_length=50,blank=True)
+
+class Hood(models.Model):
+    hood_name=models.CharField(max_length=50,blank=True)
+    hood_photo=models.ImageField(upload_to='hoodpics/')
+    population=models.CharField(max_length=50,blank=True)
+
+class Business(models.Model):
+    business_name=models.CharField(max_length=50,blank=True)
+    email=EmailField(null=False)
+    user=models.ForeignKey(User,null=True,on_delete=models.Case)
+    hood=models.ForeignKey(Hood,null=True,on_delete=models.CASCADE)
+
+class Notification(models.Model):
+    post=models.CharField(max_length=50,blank=True)
+    posted_by=models.ForeignKey(User,null=True,on_delete=models.CASCADE)
+    hood=models.ForeignKey(hood,null=True,on_delete=models.CASCADE)
+        
+
--- mtaa/urls.py
+++ mtaa/urls.py
@@ -4,6 +4,6 @@ from .import views
 
 urlpatterns=[
     url(r'^$',views.register,name = 'register'),
-    
+    url(r'^home/$',views.home, name='home')
 
 ]
