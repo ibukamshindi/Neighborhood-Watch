3c0f4223d93564062f5c50d9d61b15312acc6914
ibukamshindi <ibukamshindi@gmail.com>
2020-11-03 03:15:58 +0300

    add signals and post save

--- mtaa/models.py
+++ mtaa/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import EmailField
 from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 
@@ -40,11 +41,13 @@ class Profile(models.Model):
     
     def save_user(self):
         self.save()
-
+    
+    @receiver(post_save, sender=User)
     def create_user_profile(sender,instance,created,**kwargs):
         if created:
             Profile.objects.create(user_id=instance)
-
+    
+    @receiver(post_save, sender=User)
     def save_profile(sender,instance,**kwargs):
         instance.profile.save()    
 class Business(models.Model):
